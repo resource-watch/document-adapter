eeef5716749aaa12687b41c09f4078cb0d5f4617
rrequero <rareq1987@gmail.com>
2016-07-07 13:10:52 +0200

    add fields endpoint and change output query

--- app/src/routes/api/v1/queryRouter.js
+++ app/src/routes/api/v1/queryRouter.js
@@ -6,7 +6,7 @@ var config = require('config');
 var importerService = require('services/importerService');
 var queryService = require('services/queryService');
 var csvSerializer = require('serializers/csvSerializer');
-var mappingSerializer = require('serializers/mappingSerializer');
+var fieldSerializer = require('serializers/fieldSerializer');
 var redisClient = require('redis').createClient({port: config.get('redis.port'), host:config.get('redis.host')});
 var router = new Router({
     prefix: '/csv'
@@ -15,24 +15,30 @@ var router = new Router({
 
 class CSVRouter {
 
-    static *
-        import () {
-            logger.info('Adding csv with dataset id: ', this.request.body.connector);
-            yield importerService.addCSV(this.request.body.connector.connector_url, 'index-' + this.request.body.connector.id, this.request.body.connector.id);
-            this.body = '';
-        }
+    static * import () {
+        logger.info('Adding csv with dataset id: ', this.request.body.connector);
+        yield importerService.addCSV(this.request.body.connector.connector_url, 'index-' + this.request.body.connector.id, this.request.body.connector.id);
+        this.body = '';
+    }
 
     static * query() {
-        logger.debug(this.query);
         logger.info('Do Query with dataset', this.request.body);
 
         let result = yield queryService.doQuery(this.query.select, this.query.order,
             this.query.aggr_by, this.query.filter, this.query.filter_not, this.query.limit, this.query.aggr_columns, 'index-' + this.request.body.dataset.id, this.query.sql);
         let data = csvSerializer.serialize(result);
-        let mapping = yield queryService.getMapping('index-' + this.request.body.dataset.id);
-        data.mapping = mappingSerializer.serialize(mapping);
+        let fields = yield queryService.getMapping('index-' + this.request.body.dataset.id);
+        data.fields = fieldSerializer.serialize(fields);
         data.clone_url = CSVRouter.getCloneUrl(this.request.url, this.params.dataset);
         this.body = data;
+    }
+
+    static * query() {
+        logger.debug(this.query);
+        logger.info('Get fields of dataset', this.request.body);
+
+        let fields = yield queryService.getMapping('index-' + this.request.body.dataset.id);
+        this.body = fieldSerializer.serialize(fields);
     }
 
     static getCloneUrl(url, idDataset) {
@@ -67,6 +73,7 @@ const cacheMiddleware = function*(next){
 };
 
 router.post('/query/:dataset', cacheMiddleware, CSVRouter.query);
+router.post('/fields/:dataset', cacheMiddleware, CSVRouter.fields);
 router.post('/', CSVRouter.import);
 router.delete('/:id', CSVRouter.delete);
 
--- app/src/serializers/fieldSerializer.js
+++ app/src/serializers/fieldSerializer.js
@@ -2,20 +2,20 @@
 
 var logger = require('logger');
 
-class CSVSerializer {
+class FieldSerializer {
 
     static searchProperties(el){
         if(!el.properties && Object.keys(el).length === 1){
-            return CSVSerializer.searchProperties(el[Object.keys(el)[0]]);
+            return FieldSerializer.searchProperties(el[Object.keys(el)[0]]);
         }
         return el.properties;
     }
 
     static serialize(data) {
         if(data && data.length > 0){
-            return CSVSerializer.searchProperties(data[0]);
+            return FieldSerializer.searchProperties(data[0]);
         }
     }
 }
 
-module.exports = CSVSerializer;
+module.exports = FieldSerializer;
