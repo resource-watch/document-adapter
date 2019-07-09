a7095e5a4dd5846c9c3b5313875e757f3de4a26e
Tiago Garcia <tiagojsag@gmail.com>
2019-07-09 11:29:15 +0200

    Support reconnecting on queuing on a closed channel

--- app/src/services/taskQueueService.js
+++ app/src/services/taskQueueService.js
@@ -2,6 +2,7 @@ const logger = require('logger');
 const QueueService = require('services/queueService');
 const { task } = require('rw-doc-importer-messages');
 const config = require('config');
+const { IllegalOperationError } = require('amqplib/lib/error');
 
 class TaskQueueService extends QueueService {
 
@@ -9,13 +10,19 @@ class TaskQueueService extends QueueService {
         super(config.get('queues.tasks'));
     }
 
-    async sendMessage(msg) {
+    async sendMessage(msg, retryOnChannelClosed = true) {
         logger.info(`Sending message to ${this.q}`, msg);
         try {
             // Sending to queue
-            (await this.channel).sendToQueue(this.q, Buffer.from(JSON.stringify(msg)));
+            const channel = await this.channel;
+            channel.sendToQueue(this.q, Buffer.from(JSON.stringify(msg)));
         } catch (err) {
-            logger.error(`Error sending message to ${this.q}: ${err}`);
+            if (err instanceof IllegalOperationError && err.message === 'Channel closed' && retryOnChannelClosed) {
+                await this.init(false);
+                await this.sendMessage(msg, false);
+            } else {
+                logger.error(`Error sending message to ${this.q}: ${err}`);
+            }
         }
     }
 
